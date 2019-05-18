85d4d0f0f6a3e1b8b26d81867082a49b3deed51a
root <root@vps683807.ovh.net>
2019-05-18 23:44:06 +0200

    lolgp

--- d01/ex10/do_op.php
+++ d01/ex10/do_op.php
@@ -5,21 +5,6 @@ function enl($a) {
 	return ($a);
 }
 
-function cal($a) {
-	$r = array("/", "*", "-", "+");
-	$e = str_split($a);
-	if (count($e) != 1)
-		return (false);
-	foreach ($r as $d) 
-		if ($d == $e[0])
-			return (true);
-	return (true);
-}
-
-
-
-
-
 if ($argc == 4) {
 	$a = enl($argv[1]);
 	$b = enl($argv[2]);
@@ -27,7 +12,7 @@ if ($argc == 4) {
 	eval("\$a = $a $b $c; echo \$a;");
 
 } else {
-
+	echo "Incorrect Parameters";
 }
-
+echo "\n";
 ?>
