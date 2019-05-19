f56f6275cb43432c573494ab2a3c5dd8423ff8ea
Boris DENOYELLE <bordenoy@e1r10p7.42.fr>
2019-05-19 13:28:07 +0200

    xs

--- d02/ex03/who.php
+++ d02/ex03/who.php
@@ -1,4 +1,13 @@
 <?php
-$f = file_get_contents("/var/run/utmp", true);
-echo $f;
 
+$f = fopen("/var/run/utmpx", "r");
+$struct = 'a256user/a4id/a32line/ipid/itype/I2time/a256host/i16pad';
+if ($f) {
+	date_default_timezone_set('Europe/paris');
+	while ($read = fread($f, 628)) {
+		$ph = unpack($struct, $read);
+		if ($ph['type'] == 7 && strcmp(trim($ph['user']), get_current_user()) == 0) {
+			echo $ph["user"]." ".$ph["line"]."  ".date("M j H:i", $ph["time1"])."\n";
+		}
+	}
+}
